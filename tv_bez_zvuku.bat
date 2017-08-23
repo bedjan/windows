53a1991f78d006475be70e2299bab08eee77c3ce
bedjan <bedjan@gmail.com>
2017-08-23 11:37:37 +0200

    Add files via upload

---
+++ tv_bez_zvuku.bat
@@ -0,0 +1,3 @@
+@echo off
+echo tv_bez_zvuku
+"D:\Program Files\VideoLAN\VLC\vlc.exe" --loop --no-video "https://gist.githubusercontent.com/bedjan/92f06608c30099c8215e7ce58db204d0/raw/b0fbe836950eef6e6264d5e28a36529da5b8f781/tv_online.m3u"
