a9a40325955bd981e588ce5cd1b8517399c16c24
bedjan <bedjan@gmail.com>
2017-08-25 07:24:56 +0200

    Add files via upload

--- difm.bat
+++ difm.bat
@@ -1,3 +1,3 @@
 @echo off
 echo difm
-"D:\Program Files\VideoLAN\VLC\vlc.exe" --loop "https://gist.githubusercontent.com/sim642/4f73e3e145fd1eac8c39/raw/43dbe82623404c0f549a7391b5a618f5fd89f4dc/DI.m3u"
+"D:\Program Files\VideoLAN\VLC\vlc.exe" --loop "https://raw.githubusercontent.com/bedjan/difm/master/difm.m3u"
